02eeca116c026afe10deab02c4eea7d9a2673709
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2023-12-07 17:45:05 +0530

    map

---
+++ LAB_PROGRAMS/CPP LAB/STL/map/map_implementation.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<map>
+#include<string>
+using namespace std;
+
+int main()
+{
+        map<string,int> marks;
+        marks["eswar"]=95;
+        marks["aditya"]=93;
+        marks["sai"]=35;
+
+
+        map<string,int> :: iterator it;
+        
+        for ( it = marks.begin(); it != marks.end(); it++)
+        {
+            cout << it->first << it->second<<"\n";
+        }
+        
+  cout<<"is the map empty ,if 0 not empty: "<<marks.empty();
+
+marks.clear();
+  cout<<"is the map empty ,if 1 it is  empty: "<<marks.empty();
+
+
+
+
+}
