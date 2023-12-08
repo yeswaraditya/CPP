1c5ee3acafdf5bdbd24e1c7174b06e8530dcad62
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2023-12-08 15:50:44 +0530

    formt

--- LAB_PROGRAMS/2nd_midexam_questions/2.1.cpp
+++ LAB_PROGRAMS/2nd_midexam_questions/2.1.cpp
@@ -1,23 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-template<class T>
+template <class T>
 
-T find_mean(T a[],int size)
+T find_mean(T a[], int size)
 {
-    T sum=0;
-     for(int i=0;i<size;i++)
-     {
-        sum=sum+a[i];
-     }
+    T sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum = sum + a[i];
+    }
 
-     T mean=sum/size;
+    T mean = sum / size;
 
-     return mean;
+    return mean;
 }
 
-
-
-int main() {
+int main()
+{
     int a[] = {3, 2, 1, 4, 34, 21, 67, 45, 87};
     float f[] = {2.3f, 4.5f, 5.6f, 7.8f, 12.3f};
     double d[] = {3.2, 7.9, 5.8, 3.1, 13.6, 11.3};
