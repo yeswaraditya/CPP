f940cc097e3dd2b160bebf7a7d7c511aacff42af
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2023-12-08 15:51:41 +0530

    formt

--- LAB_PROGRAMS/CPP LAB/constructors/parameterised_constru_inheritance.cpp
+++ LAB_PROGRAMS/CPP LAB/constructors/parameterised_constru_inheritance.cpp
@@ -1,51 +1,48 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
 class Person
 {
-    private:
+private:
     string name;
     int age;
 
-    public:
-     Person(string name,int age)
-     {
-        this->name=name;
-        this->age=age;
-     }
-
-     void display()
+public:
+    Person(string name, int age)
     {
-        cout<<"Name: "<<name<<endl;
-        cout<<"Age: "<<age<<endl;
+        this->name = name;
+        this->age = age;
+    }
+
+    void display()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
     }
 };
-class Student :public Person
+class Student : public Person
 {
-    private:
+private:
     string coursename;
 
-    public:
-
-    Student(string name,int age,string coursename):Person(name,age)
+public:
+    Student(string name, int age, string coursename) : Person(name, age)
     {
-        this->coursename=coursename;
-
+        this->coursename = coursename;
     }
     void displayStudentDetails()
     {
         display();
-        cout<<"coursename: "<<coursename<<endl;
-
+        cout << "coursename: " << coursename << endl;
     }
 };
 
 int main()
 {
-    Person person("eswar",19);
+    Person person("eswar", 19);
     person.display();
 
-    Student s("aditya",21,"cse course");
+    Student s("aditya", 21, "cse course");
     s.displayStudentDetails();
 }
