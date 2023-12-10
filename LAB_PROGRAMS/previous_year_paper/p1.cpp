225a971e5851c0f3268e40e30402bf8204d499b8
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2023-12-10 23:26:54 +0530

    update

--- LAB_PROGRAMS/previous_year_paper/p1.cpp
+++ LAB_PROGRAMS/previous_year_paper/p1.cpp
@@ -1,3 +1,18 @@
 /* Develop a program to insert an element into the vector object. Use member function and iterator to traverse.*/
 #include<iostream>
+#include<vector>
 using namespace std;
+
+int main()
+{
+   vector<int> v;
+   v.push_back(20);
+   v.push_back(23);
+   v.push_back(2);
+   v.push_back(50);
+
+   for(auto element:v)
+   {
+    cout<<element<<endl;
+   }
+}
