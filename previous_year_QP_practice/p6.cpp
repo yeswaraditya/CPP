debf7e07699f282d88992264230b44512e94e680
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2023-12-17 19:20:40 +0530

    p7

--- previous_year_QP_practice/p6.cpp
+++ previous_year_QP_practice/p6.cpp
@@ -4,15 +4,20 @@
 
  #include<iostream>
  using namespace std;
-
+ const int max_size= 100 ;
  class Matrix
  {
+    private:
+    int size;
+    int m[max_size][max_size];
+
+    
     public:
     
     
     void read( int n)
     {
-        int m[n][n];
+        size=n;
         
         for (int  i = 0; i < n; i++)
         {
@@ -26,13 +31,35 @@
         
     }
 
-    void addMatrix(int mat1[n][n],int mat2[n][n])
+    Matrix operator +(Matrix ma)
     {
+        Matrix temp;
+        
+        for (int  i = 0; i < size; i++)
+        {
+            for (int  j = 0; j < i; j++)
+            {
+                temp.m[i][j]= m[i][j]+ma.m[i][j];
+            }
+            
+        }
 
+        return temp;
+        
+          
     }
 
     void display()
     {
+        for (int  i = 0; i < size; i++)
+        {
+            for (int  j = 0; j < i; j++)
+            {
+               cout<<m[i][j]<<" ";
+            }
+            cout<<"\n";
+            
+        }
 
     }
 
@@ -52,9 +79,16 @@
     mat2.read(n);
 
 
-   result.addMatrix(mat1,mat2);
+   result= mat1+mat2;
 
-    
+   cout<<"matrix 1"<<endl;
+   mat1.display();
+
+   cout<<"matrix 2"<<endl;
+   mat2.display();
+
+    cout<<"sum of matrix is "<<endl;
+    result.display();
     
 
   return 0;
---
+++ previous_year_QP_practice/p7.cpp
@@ -0,0 +1,3 @@
+/* 
+“Only one copy of the static variable is created for an entire class and is shared by all the objects of that class,
+ no matter how many objects are created”. Justify this statement with example program*/
